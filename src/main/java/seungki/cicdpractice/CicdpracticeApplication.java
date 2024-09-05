cfaf9e288a25f1f12eae490eae3c3626fb353453
seungki1011 <kimseungki1011@gmail.com>
2024-09-06 00:06:28 +0900

    Init: initialize project

---
+++ src/main/java/seungki/cicdpractice/CicdpracticeApplication.java
@@ -0,0 +1,13 @@
+package seungki.cicdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpracticeApplication.class, args);
+	}
+
+}
