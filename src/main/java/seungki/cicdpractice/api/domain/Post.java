b25e1f3b99d32f8379c22cf35f98b7ab0c6c598e
seungki1011 <kimseungki1011@gmail.com>
2024-09-09 20:39:12 +0900

    Feat: Post에 equals, hashCode 오버라이딩

--- src/main/java/seungki/cicdpractice/api/domain/Post.java
+++ src/main/java/seungki/cicdpractice/api/domain/Post.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,5 +27,22 @@ public class Post {
         this.id = id;
         this.title = title;
         this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
